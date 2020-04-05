7ea77ef9fca3bff6b1cc9dae5139e007dcd6df13
Guoth <935809915@qq.com>
2020-04-05 15:28:58 +0800

    视频路径

--- src/controller/mpageController.js
+++ src/controller/mpageController.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
-
+const moment = require('moment')
 router.get('/', function (req, res) {
+    console.log("时间 " + moment().format('LLL') + " 路径 ", req.url);
     res.render('mainpage.html')
 })
 
