dbe94bc87e90fb267e09797e4976d9f163b614d0
Guoth <935809915@qq.com>
2020-04-11 12:06:46 +0800

    服务端渲染教程

--- frontend/src/controller/mpageController.js
+++ frontend/src/controller/mpageController.js
@@ -3,22 +3,46 @@ const router = express.Router()
 const httpUtils = require('../utils/httpUtils')
 
 router.get('/', function (req, res) {
+    console.log('访问首页');
     res.render('mainpage.html')
 })
 router.get('/ios', function (req, res) {
-    res.render('ios.html')
+    res.render('framework.html', {
+        tmpl: './tmpls/iosapps.tmpl.html'
+    })
 })
 router.get('/kefu', function (req, res) {
-    res.render('kefu.html')
+    res.render('framework.html', {
+        tmpl: './tmpls/kefu.tmpl.html'
+    })
 })
 router.get('/bidu', function (req, res) {
-    res.render('how.html')
+    res.render('framework.html', {
+        tmpl: './tmpls/how.tmpl.html'
+    })
+})
+router.get('/jiaocheng/:id', function (req, res) {
+    const id = req.params.id
+    console.log('查询教程 id:', id);
+
+    httpUtils.sendRequest('localhost', `/quickstart/detail.do/${id}`, 'GET', 8445, (data) => {
+        let result = JSON.parse(data.toString('utf8'))
+        console.log('请求结果', result);
+
+        res.render('framework.html', {
+            tmpl: './tmpls/toturial.tmpl.html',
+            toturial: {
+                ...result
+            }
+        })
+    })
 })
 router.get('/xinshou', function (req, res) {
+    console.log('新手页面');
     httpUtils.sendRequest('localhost', '/quickstart/condition.do', 'GET', 8445, (data) => {
         let result = JSON.parse(data.toString('utf8'))
         res.render('framework.html', {
-            tmpl: './tmpls/toturial.tmpl.html',
+            tmpl: './tmpls/toturiallist.tmpl.html',
             data: {
                 toturials: result.data
             }
