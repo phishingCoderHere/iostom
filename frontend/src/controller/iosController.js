0024a88c9deaaa7857fc0b16a89738ec90dd8172
Guoth <935809915@qq.com>
2020-04-11 09:27:25 +0800

    新手教程

--- frontend/src/controller/iosController.js
+++ frontend/src/controller/iosController.js
@@ -1,14 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/ios', function (req, res) {
-    res.render('ios.html')
-})
-router.get('/kefu', function (req, res) {
-    res.render('kefu.html')
-})
-router.get('/bidu', function (req, res) {
-    res.render('App试玩赚钱怎么做？新手篇_苹果宝盒网.html')
-})
+
 
 module.exports = router;
--- frontend/src/controller/mpageController.js
+++ frontend/src/controller/mpageController.js
@@ -4,5 +4,16 @@ const router = express.Router()
 router.get('/', function (req, res) {
     res.render('mainpage.html')
 })
-
+router.get('/ios', function (req, res) {
+    res.render('ios.html')
+})
+router.get('/kefu', function (req, res) {
+    res.render('kefu.html')
+})
+router.get('/bidu', function (req, res) {
+    res.render('how.html')
+})
+router.get('/xinshou', function (req, res) {
+    res.render('tutorial.html')
+})
 module.exports = router;
