d28ef2f2b0676130a94a1100b159e070bcf921e9
Guoth <935809915@qq.com>
2020-04-11 10:33:05 +0800

    套模板

--- frontend/src/controller/mpageController.js
+++ frontend/src/controller/mpageController.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const httpUtils = require('../utils/httpUtils')
 
 router.get('/', function (req, res) {
     res.render('mainpage.html')
@@ -14,6 +15,14 @@ router.get('/bidu', function (req, res) {
     res.render('how.html')
 })
 router.get('/xinshou', function (req, res) {
-    res.render('tutorial.html')
+    httpUtils.sendRequest('localhost', '/quickstart/condition.do', 'GET', 8445, (data) => {
+        let result = JSON.parse(data.toString('utf8'))
+        res.render('framework.html', {
+            tmpl: './tmpls/toturial.tmpl.html',
+            data: {
+                toturials: result.data
+            }
+        })
+    })
 })
 module.exports = router;
